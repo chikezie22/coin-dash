bf308dd4eb688f3455e68d8ff947589e5b111480
Chikezie Simon <simonchikezie44@gmail.com>
2025-07-26 15:28:11 +0100

    feat: enhance UI responsiveness and touch interactions for coin cards and header

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ const App = () => {
   return (
     <div>
       <Header iimit={limit} setLimit={setLimit} />
-      <div className="max-w-[1440px] px-5 py-2.5 space-y-5">
+      <div className="max-w-[1440px] px-2.5 lg:px-5 py-2.5 space-y-5">
         <div className="flex justify-center gap-2.5 sm:justify-between items-center flex-wrap ">
           <h1 className="text-3xl font-bold">Coin Dash 🚀</h1>
           <div className="font-medium text-slate-600 flex gap-2.5 text-nowrap max-sm:text-sm">
@@ -49,7 +49,7 @@ const App = () => {
           {isLoading ? (
             <p>Loading .....</p>
           ) : (
-            <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-2.5 mx-auto place-content-center">
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-2.5 mx-auto lg:place-content-center">
               {coins.map((coin) => (
                 <CoinCard coin={coin} key={coin.id} />
               ))}
